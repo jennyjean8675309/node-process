5b363b39076c185bcd0c58233be027df49c33091
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2020-02-19 17:15:15 -0500

    added some comments to the stdin.js file

--- stdin.js
+++ stdin.js
@@ -1,8 +1,11 @@
 process.stdin.setEncoding('utf8')
 
 process.stdin.on('readable', function() {
+  // 'readable' is an event listener that waits for readable code in the stdin stream
   var chunk = process.stdin.read()
+  // we save that readable chunk of code in a variable
   if (chunk !== null) {
+    // and then print it out (using stdout.write) 
     process.stdout.write(`data: ${chunk}`)
   }
 })
